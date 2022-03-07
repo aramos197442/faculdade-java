c895da0b9d6c4c2a8bc273e97f7eefc975ceaa7a
aramos197442 <aramos197442@gmail.com>
2022-03-07 20:37:59 -0300

    Exercicio em 07-03-2022

---
+++ Exercicios/Cadastro.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class Cadastro {
+	private String[] nomes = {"","","","",""};
+	
+	public void incluir() {
+		for(int i=0; i<nomes.length; i++) {
+			nomes[i] = (String) JOptionPane.showInputDialog("Entre com um nome: ");
+		}
+	}
+	
+	public void listar() {
+		for(int i=0; i<nomes.length; i++) {
+			JOptionPane.showMessageDialog(null, nomes[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cadastro nome = new Cadastro();
+		boolean sai = true;
+		String menu = "1 - Incluir\n2 - Listar\n3 - Sair";
+		while(sai) {
+			String inputValue = JOptionPane.showInputDialog(menu);
+			switch (inputValue) {
+			  case "1":
+				nome.incluir();
+				break;
+			  case "2":
+			    nome.listar();
+			    break;
+			  case "3":
+			    sai=false;
+			    break;
+/*			  default:
+				  JOptionPane.showMessageDialog(null, "Escolha 1, 2 ou 3 apenas","alerta", JOptionPane.ERROR_MESSAGE);
+				  break;
+*/			}
+			
+		}
+		
+		
+
+	}
+
+
+
+}
