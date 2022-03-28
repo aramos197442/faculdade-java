9ff63c189da344514deff6eba8153535f1bc283a
aramos197442 <aramos197442@gmail.com>
2022-03-28 15:36:34 -0300

    Ajuste na Classe Conexao

--- revisao4/src/main/java/local/uninga/ads/revisao4/Conexao.java
+++ revisao4/src/main/java/local/uninga/ads/revisao4/Conexao.java
@@ -1,20 +1,22 @@
 package local.uninga.ads.revisao4;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager;																																			
 import java.sql.SQLException;
 
 public class Conexao {    
     public static String status = "";
-    public static Connection getConnection(){
-        Connection con = null;
+    public static Connection getConnection(){																																					
+        Connection con = null;																								
         try{
             //Class.forName("org.postgresql.Driver").newInstance();
-            String url = "jdbc:mysql://127.0.0.1/uninga";
-            con = DriverManager.getConnection(url,"mysql","");
+            String url = "jdbc:postgresql://127.0.0.1:5432/postgres";
+            con = DriverManager.getConnection(url,"postgres","postgres");
             status = "Conexão aberta!";
         }catch(SQLException e){
             status = e.getMessage();
+/*        }catch(ClassNotFoundException e){
+            status = e.getMessage(); */
         }catch(Exception e){
             status = e.getMessage();
         }
